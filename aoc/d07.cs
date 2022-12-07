f1ff32d50feb4918dc563a1eb0a738dd82bb7f05
Łukasz Łobacz <lulo@demant.com>
2022-12-07 14:25:40 +0100

    code improvement

--- aoc/d07.cs
+++ aoc/d07.cs
@@ -7,49 +7,43 @@ abstract class d07
 {
     protected List<Dir7> dirs = new List<Dir7>();
     protected Dir7 tree = new Dir7("/");
-    protected Dir7? current;
+    protected Dir7 current;
+
+    public d07() { current = tree; }
 
     public void Run()
     {
-        var lines = File.ReadLines(@"..\..\..\07.txt").Skip(1).ToArray();
-        current = tree;
+        var lines = File.ReadLines(@"..\..\..\07.txt").Skip(1).ToList();
 
-        foreach (var line in lines)
-        {
-            var words = line.Split(' ');
-            if (words[0] == "dir")
+        lines.ForEach(line => 
+        { 
+            switch (line.Split(' '))
             {
-                current.TryCreateDir(words[1]);
-                continue;
-            }
+                case string[] words when words[0] == "dir": 
+                    current.TryCreateDir(words[1]); 
+                    break;
 
-            if (char.IsDigit(words[0][0]))
-            {
-                current.TryCreateFile(Convert.ToInt32(words[0]), words[1]);
-                continue;
-            }
+                case string[] words when char.IsDigit(words[0][0]): 
+                    current.TryCreateFile(Convert.ToInt32(words[0]), words[1]); 
+                    break;
 
-            if (words[0] == "$" && words[1] == "cd")
-            {
-                current = words[2] != ".." ? current.Dirs[words[2]] : current.Parent ?? throw new NullReferenceException("root");
-                continue;
-            }
-        }
-
-        void Traverse(Dir7 dir)
-        {
-            if (Filter(dir))
-            {
-                dirs.Add(dir);
-            }
-            foreach (var _dir in dir.Dirs)
-            {
-                Traverse(_dir.Value);
-            }
-        }
+                case string[] words when words[0] == "$" && words[1] == "cd":
+                    current = words[2] != ".." ? current.Dirs[words[2]] : current.Parent ?? throw new NullReferenceException("root"); 
+                    break;
+            };
+        });
 
         Traverse(tree);
         Console.WriteLine(Result);
+    }
+
+    void Traverse(Dir7 dir)
+    {
+        if (Filter(dir))
+        {
+            dirs.Add(dir);
+        }
+        dir.Dirs.Values.ToList().ForEach(Traverse);
     }
 
     abstract protected int Result { get; }
@@ -58,6 +52,8 @@ abstract class d07
 
 public class Dir7
 {
+    private int? sizeCache;
+
     public string Name;
     public Dir7? Parent;
 
@@ -71,15 +67,8 @@ public class Dir7
 
     public void TryCreateFile(int size, string name) => Files[name] = size;
 
-    public int Size => Dirs.Sum(x => x.Value.Size) + Files.Sum(x => x.Value);
-}
-
-class File7
-{   
-    public string Name;
-    public int Size;
-
-    public File7(string name, int size) { Name = name; Size = size; }
+    public int Size => sizeCache ??= size;
+    private int size => Dirs.Sum(x => x.Value.Size) + Files.Sum(x => x.Value);
 }
 
 class d07_1 : d07
