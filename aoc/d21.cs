0e2ed2b8de9be41edb028434f4c15287390c8b7a
Łukasz Łobacz <lulo@demant.com>
2022-12-22 00:04:18 +0100

    day 21

--- aoc/d21.cs
+++ aoc/d21.cs
@@ -1,6 +1,8 @@
 ﻿abstract class d21
 {
     HashSet<MonkeyInfo> monkeys = new HashSet<MonkeyInfo>();
+    
+    protected abstract void Result(MonkeyNode root, MonkeyNode humnNode);
 
     public void Run()
     {
@@ -12,9 +14,9 @@
         }
         var rootMonkeyInfo = monkeys.Single(x => x.name== "root");
         monkeys.Remove(rootMonkeyInfo);
-        var rootNode = new MonkeyNode(monkeys, rootMonkeyInfo);
-        var result = rootNode.Value();
-        Console.WriteLine(result);
+        MonkeyNode humnNode = null;
+        var rootNode = new MonkeyNode(monkeys, default, rootMonkeyInfo, ref humnNode);
+        Result(rootNode, humnNode);
     }
 }
 
@@ -29,13 +31,29 @@ class MonkeyNode
         { '/', (l, r) => l / r },
     };
 
+    private Dictionary<char, Func<long, long, long>> revOps = new Dictionary<char, Func<long, long, long>>()
+    {
+        { '+', (a, b) => a - b },
+        { '-', (a, b) => a + b },
+        { '*', (a, b) => a / b },
+        { '/', (a, b) => a * b },
+    };
+
     public string Name { get; init; }
     public bool IsNumber { get; init; }
 
+    public MonkeyNode? Parent { get; init; }
     public long NumberValue { get; init; }
 
-    public MonkeyNode(HashSet<MonkeyInfo> monkeys,MonkeyInfo info)
+    public bool CalcIsNormalNode { get; set; } = true;
+    public long CalcValue { get; set; }
+    public MonkeyNode CalcRevNode => !left.CalcIsNormalNode ? left : right;
+    public MonkeyNode CalcNormalNode => left.CalcIsNormalNode ? left : right;
+
+    public MonkeyNode(HashSet<MonkeyInfo> monkeys, MonkeyNode? parent, MonkeyInfo info, ref MonkeyNode humnNode)
     {
+        Parent = parent;
+
         var body = info.body;
         Name = info.name;
         IsNumber = char.IsNumber(body[0]);
@@ -43,12 +61,16 @@ class MonkeyNode
         {
             op = body.Split(' ')[1][0];
             var leftName = body.Split(' ')[0];
-            left = new MonkeyNode(monkeys, monkeys.Single(x => x.name == leftName));
+            left = new MonkeyNode(monkeys, this, monkeys.Single(x => x.name == leftName), ref humnNode);
             var rightName = body.Split(' ')[2];
-            right = new MonkeyNode(monkeys, monkeys.Single(x => x.name == rightName));
+            right = new MonkeyNode(monkeys, this, monkeys.Single(x => x.name == rightName), ref humnNode);
         }
         else
         {
+            if (Name == "humn")
+            {
+                humnNode = this;
+            }
             NumberValue = long.Parse(body);
         }
 
@@ -65,18 +87,53 @@ class MonkeyNode
         return ops[op](left.Value(), right.Value());
     }
 
+    public long CalcRevValue(long value) => revOps[op](CalcValue, value);
+
     MonkeyNode? left;
     MonkeyNode? right;
 }
 
 class d21_1 : d21
 {
-
+    protected override void Result(MonkeyNode root, MonkeyNode humnNode)
+    {
+        var result = root.Value();
+        Console.WriteLine(result);
+    }
 }
 
 class d21_2 : d21
 {
+    protected override void Result(MonkeyNode root, MonkeyNode humnNode)
+    {
+        _ = root.Value();
+        var firstNode = PrepareHumnBranch(humnNode);
+        firstNode.CalcValue = -root.CalcNormalNode.Value();
+        CalcHumnValue(firstNode);
+        Console.WriteLine(humnNode.CalcValue);
+    }
 
+    MonkeyNode PrepareHumnBranch(MonkeyNode node)
+    {
+        do
+        {
+            node.CalcIsNormalNode = false;
+            node = node.Parent;
+        }
+        while (node.Name != "root");
+
+        return node.CalcRevNode;
+    }
+
+    void CalcHumnValue(MonkeyNode node)
+    {
+        do
+        {
+            node.CalcRevNode.CalcValue = node.CalcRevValue(node.CalcNormalNode.Value());
+            node = node.CalcRevNode;
+        }
+        while (node.Name != "humn");
+    }
 }
 
 record struct MonkeyInfo(string name, string body);
