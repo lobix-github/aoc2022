3a3198ccb2723df5d8fef174f706e31d01036534
luk81luk81 <luk81luk81@gmail.com>
2022-12-23 22:39:18 +0100

    day 17 / 2

--- aoc/d17.cs
+++ aoc/d17.cs
@@ -1,10 +1,12 @@
 ﻿public abstract class d17
 {
     HashSet<CavePoint> cave = new HashSet<CavePoint>() { new (0, -1), new(1, -1), new(2, -1), new(3, -1), new(4, -1), new(5, -1), new(6, -1) };
+    Dictionary<int, int> cyclesHeights = new Dictionary<int, int>();
 
     //string jetPattern = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
-    string jetPattern => File.ReadAllText(@"..\..\..\17.txt");
-    protected abstract long rounds { get; }
+    string _jetPattern;
+    string jetPattern => _jetPattern ??= File.ReadAllText(@"..\..\..\17.txt");
+    protected abstract long rounds { get; set; }
 
     public void Run()
     {
@@ -13,12 +15,69 @@
         while (count < rounds)
         {
             var lowestY = cave.Max(p => p.y);
-            var rock = RockFabric.GetRock(lowestY + 1, count++);
+            (var patternHeight, var patternCycles) = MaybePattern(count, lowestY);
+            if (patternHeight != default)
+            {
+                var reps = (rounds - count) / patternCycles;
+                var skippedHeight = reps * patternHeight;
+                var skippedRounds = reps * patternCycles;
+                var fakeCycle = count - patternCycles;
+                while (count < rounds - skippedRounds)
+                {
+                    skippedHeight += cyclesHeights[fakeCycle++];
+                    count++;
+                }
+
+                var r = lowestY + skippedHeight + 1;
+            }
+
+            var rock = RockFabric.GetRock(lowestY + 1, count);
             while(rock.Fall(cave, jetFabric(step++)));
+            cyclesHeights[count] = cave.Max(p => p.y) - lowestY;
+            count++;
         }
 
         var result = cave.Max(p => p.y) + 1;
         Console.WriteLine(result);
+    }
+
+    int prevRockIndex = -1;
+    int prevHeight;
+    List<(int, int)> deltas = new List<(int, int)>();
+    (int, int) MaybePattern(int cycle, int lowestY)
+    {
+        if (cycle is > 0 && Enumerable.Range(0, 6).All(x => cave.Contains(new CavePoint(x, lowestY))))
+        {
+            var rockIndex = cycle % 5;
+            if (prevRockIndex is -1)
+            {
+                prevRockIndex = rockIndex;
+                prevHeight = lowestY;
+            }
+            else if (prevRockIndex == rockIndex)
+            {
+                var delta = lowestY - prevHeight;
+                deltas.Add((delta, cycle));
+                prevHeight = lowestY;
+                if (deltas.Count % 2 is 0)
+                {
+                    // maybe pattern? let's check!
+                    bool isPattern = true;
+                    for (int i = 0; i < deltas.Count / 2; i++)
+                    {
+                        isPattern &= deltas[i].Item1 == deltas[deltas.Count / 2 + i].Item1;
+                    }
+
+                    if (isPattern)
+                    {
+                        // we have a pattern!
+                        return (deltas.Sum(x => x.Item1) / 2, deltas[deltas.Count / 2].Item2 - deltas[0].Item2);
+                    }
+                }
+            }
+        }
+
+        return default;
     }
 
     int jetFabric(int i) => jetPattern[i % jetPattern.Length] == '>' ? 1 : -1;
@@ -80,12 +139,12 @@ class Rock
 
 class d17_1 : d17
 {
-    protected override long rounds => 2022;
+    protected override long rounds { get; set; } = 2022;
 }
 
 class d17_2 : d17
 {
-    protected override long rounds => 1_000_000_000_000;
+    protected override long rounds { get; set; } = 1_000_000_000_000;
 }
 
 static class RockFabric
